ffdf48ab7e5de716ffd49e0ccb73b21c584591a4
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-15 12:43:12 -0800

    added roles in database

--- data/migrations/20201214143720_create_users.js
+++ data/migrations/20201214143720_create_users.js
@@ -1,11 +1,21 @@
-const e = require("express");
 
 exports.up = function(knex) {
     return knex.schema
+    .createTable('roles', table => {
+        table.increments()
+        table.string('name', 128).notNullable().unique();
+    })
     .createTable('users', table => {
         table.increments()
         table.string('username', 128).notNullable().unique();
         table.string('password', 128).notNullable()
+        //Role: role of the user
+        table.integer('role')
+        .unsigned()
+        .references("roles.id")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT")
+        .defaultTo(2);
     })
 };
 
--- data/seeds/init.js
+++ data/seeds/init.js
@@ -1,10 +1,9 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
   return knex('users').truncate()
     .then(function () {
       return knex('users').insert([
-        {username: 'joseph'}
+        {username: 'joseph' }
       ]);
     });
 };
